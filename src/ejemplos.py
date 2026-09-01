78d58ffcc2f1a6ee163fea50d754cfb035244647
Campus Sala 203 <casala2@usantotomas.red>
2026-09-01 14:22:16 -0500

    Creacion archivo de ejmplo

---
+++ src/ejemplos.py
@@ -0,0 +1,23 @@
+print ("helo world")
+
+#creacion de variables
+x = 15
+y = 10
+
+z = y - x
+
+print (z)
+
+z = y/(x+4)
+print(z)
+
+PI = 3.1416
+radio = 4
+
+area = PI * radio**2
+
+print (area)
+
+perimetro = PI * radio 
+
+print (perimetro)
